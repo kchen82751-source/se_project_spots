babc54ee798e9446118247e2ac01513d78eba98c
PracticumTutor <PracticumTutor@gmail.com>
2026-03-20 14:53:25 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const addBtn = document.querySelector(".profile__add-btn");
 const addModal = document.querySelector("#new-post-modal");
 const addCloseBtn = addModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +32,11 @@ addBtn.addEventListener("click", function () {
 addCloseBtn.addEventListener("click", function () {
   addModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
